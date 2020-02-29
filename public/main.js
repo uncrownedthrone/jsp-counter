bf3f82ab26ffaa6abdfa116ee94b90d87e29e730
Justin Swiger <justin.swiger@me.com>
2020-02-29 15:01:24 -0500

    basic counter done

--- public/main.js
+++ public/main.js
@@ -1,7 +1,38 @@
-const main = () => {
-  if (document.querySelector('h1')) {
-    document.querySelector('h1').textContent = "We're going to make a counter."
+const qs = e => document.querySelector(e)
+
+let score = 0
+
+const totalScoreText = qs('.total-score')
+totalScoreText.textContent = score
+
+const addOne = () => {
+  score += 1
+  totalScoreText.textContent = score
+  if (score === 21) {
+    qs('.total-score').textContent = 'Winner!'
+    qs('button.add').disabled = true
+    qs('button.subtract').disabled = true
   }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const subtractOne = () => {
+  score -= 1
+  totalScoreText.textContent = score
+  if (score === 21) {
+    qs('.total-score').textContent = 'Winner!'
+    qs('button.add').disabled = true
+    qs('button.subtract').disabled = true
+  }
+}
+
+const resetGame = () => {
+  const resetMode = 0
+  score = 0
+  qs('button.add').disabled = false
+  qs('button.subtract').disabled = false
+  qs('.total-score').textContent = resetMode
+}
+
+qs('button.add').addEventListener('click', addOne)
+qs('button.subtract').addEventListener('click', subtractOne)
+qs('button.reset').addEventListener('click', resetGame)
